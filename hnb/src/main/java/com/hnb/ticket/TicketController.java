07671eaa539901ad67ad1807ddf09e761172ec2c
HB <HB@HB-PC>
2015-12-09 12:01:39 +0900

    xcvg df

--- hnb/src/main/java/com/hnb/ticket/TicketController.java
+++ hnb/src/main/java/com/hnb/ticket/TicketController.java
@@ -1,10 +1,31 @@
 package com.hnb.ticket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/ticket")
 public class TicketController {
+	private static final Logger logger = LoggerFactory.getLogger(TicketController.class);
+	@Autowired
+	TicketServiceImpl service;
+	@Autowired
+	TicketVO ticket;
+	
+	@RequestMapping("/Ticket")
+	public String ticket(){
+		
+		return "ticket/Ticket";
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
