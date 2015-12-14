15d66a7018ad8b881775ec9e3576481f37c13f08
HB <HB@HB-PC>
2015-12-14 10:24:26 +0900

    asd

--- hnb/src/main/java/com/hnb/event/EventController.java
+++ hnb/src/main/java/com/hnb/event/EventController.java
@@ -1,8 +1,19 @@
 package com.hnb.event;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/event")
 public class EventController {
-
+	private static final Logger logger = LoggerFactory.getLogger(EventController.class);
+	
+	@RequestMapping("/article")
+	public String article(){
+		logger.info("EventController - article() 진입");
+		
+		return "event/article.tiles";
+	}
 }
--- hnb/src/main/webapp/WEB-INF/views/global/header.jsp
+++ hnb/src/main/webapp/WEB-INF/views/global/header.jsp
@@ -125,7 +125,7 @@
 		
 		$("#event_btn").click(function() {
 			history.pushState("Event_home","","");
-			$("#box").load();
+			location.href = "${context}/event/article";
 		});
 		
 		/* 로그인 버튼 */
